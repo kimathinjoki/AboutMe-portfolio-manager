45fee12d0769089b43e7e943f267d0726aa85e26
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-01 03:54:45 -0800

    add user columns

--- db/migrate/20230228131031_create_users.rb
+++ db/migrate/20230228131031_create_users.rb
@@ -5,6 +5,15 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :name, null: false
       t.string :email, null: false, index: {unique: true, name: 'uk_email'}
       t.string :password_hash, null: false
+
+      t.string :prof_pic
+      t.string :profession
+      t.string :education
+      t.string :address
+      t.string :experience
+      t.string :skills
+      t.integer :contacts
+
       t.timestamps
     end
   end
