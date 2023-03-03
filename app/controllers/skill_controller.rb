2b006a9539aaac6df6d0e0527bc8fefe3e6766ab
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-03 01:55:01 -0800

    add skill create method

--- app/controllers/skill_controller.rb
+++ app/controllers/skill_controller.rb
@@ -2,8 +2,48 @@ class SkillController < AppController
 
     set :views, '../views/project.erb'
 
-    get '/skills' do
+
+    get '/skills' do 
+        skills = Skill.all
+        skills.to_json
+    end
+
+    post '/skills/create' do
+        data = JSON.parse(request.body.read)
+
+        begin
+            skill = Skill.create(data)
+            skill.to_json
+        rescue => e
+            { error: e.message }.to_json
+        end
         
+    end
+
+    # gets the sills of a certain user and limits them to max of 10
+    get '/skills/:id' do
+        user = User.find(self.user_id)
+        skillSet = user.skills.first(2)
+        skillSet.to_json
+    end
+
+    # adds a new skill for a specifies user
+    post 'skill/create' do
+        data = JSON.parse(request.body.read)
+
+        begin
+            skill = Skill.create(data)
+            skill.to_json
+        rescue => e
+            { error: e.message }.to_json
+        end
+    end
+
+
+    private
+
+    def user_id
+        params['id'].to_i
     end
 
 
