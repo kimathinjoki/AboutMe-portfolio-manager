cf31824d10c9ac11bad887a63f2c5280e7b0c268
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-05 16:16:39 -0800

    added a skill extention

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,9 +1,8 @@
-# frozen_string_literal: true
 
 class AppController < Sinatra::Base
 
 
-  # @api: Enable CORS Headers
+  # Enable CORS Headers
   configure do
     enable :cross_origin
   end
@@ -19,7 +18,7 @@ class AppController < Sinatra::Base
     200
   end
 
-  # @api: Format the json response
+  # Format the json response
   def json_response(code: 200, data: nil)
     status = [200, 201].include?(code) ? "SUCCESS" : "FAILED"
     headers['Content-Type'] = 'application/json'
@@ -28,23 +27,23 @@ class AppController < Sinatra::Base
     end
   end
 
-  # @api: Format all common JSON error responses
+  # Format all common JSON error responses
   def error_response(code, e)
     json_response(code: code, data: { error: e.message })
   end
 
-  # @views: Format the erb responses
+  # Format the erb responses
   def erb_response(file)
     headers['Content-Type'] = 'text/html'
     erb file
   end
 
-  # @helper: not found error formatter
+  # not found error formatter
   def not_found_response
     json_response(code: 404, data: { error: "You seem lost. That route does not exist." })
   end
 
-  # @api: 404 handler
+  # 404 handler
   not_found do
     not_found_response
   end
